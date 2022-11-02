e453ca6836fe6f40c3d82105606d39f8eb1221b1
Adham Nour <adhamnourelwaffaa@gmail.com>
2022-11-02 19:40:23 +0200

    finished the main sturcutre of the loading button

--- starter/app/src/main/java/com/udacity/LoadingButton.kt
+++ starter/app/src/main/java/com/udacity/LoadingButton.kt
@@ -10,6 +10,7 @@ import android.graphics.RectF
 import android.graphics.Typeface
 import android.util.AttributeSet
 import android.view.View
+import androidx.core.content.ContextCompat
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
@@ -17,17 +18,20 @@ class LoadingButton @JvmOverloads constructor(
 ) : View(context, attrs, defStyleAttr) {
     private var widthSize = 0
     private var heightSize = 0
+    private var textWidth = 0f
+    private var textSizing = resources.getDimension(R.dimen.default_text_size)
+    private var circleHorizontalOffset = textSizing/2
+
+    private val progress = 0f;
 
     private val valueAnimator = ValueAnimator()
 
     private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
 
     }
-    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
-        style = Paint.Style.FILL
-        textAlign = Paint.Align.CENTER
-        textSize = 55.0f
-        typeface = Typeface.create( "", Typeface.BOLD)
+    private val paint = Paint().apply {
+        isAntiAlias = true;
+        textSize=textSizing
     }
 
     init {
@@ -37,14 +41,39 @@ class LoadingButton @JvmOverloads constructor(
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        paint.color=Color.CYAN
+        if (canvas != null) {
+            drawBackgroundColor(canvas)
+            drawProgressBackground(canvas)
+            drawTitle(canvas)
+            drawCirclarProgress(canvas)
 
-        val rectF = RectF(0f,heightSize.toFloat(),widthSize.toFloat(),0f)
-        canvas?.drawRoundRect(rectF,(widthSize/10).toFloat(), ((heightSize/10).toFloat()),paint)
-        paint.color= Color.RED
-        canvas?.drawText("Download", (widthSize/2).toFloat(), (heightSize/2).toFloat()+15f,paint)
-        paint.color = Color.YELLOW
-        canvas?.drawCircle((3*widthSize/4).toFloat(),(heightSize/2).toFloat(),40f,paint)
+        }
+    }
+
+    private fun drawTitle(canvas: Canvas) {
+        paint.color = Color.WHITE
+        textWidth = paint.measureText("Download")
+        canvas.drawText("Download", widthSize / 2 - textWidth / 2, heightSize / 2 - (paint.descent() + paint.ascent()) / 2, paint)
+
+    }
+
+    private fun drawCirclarProgress(canvas: Canvas) {
+        canvas.save()
+        canvas.translate(widthSize / 2 + textWidth / 2 + circleHorizontalOffset, heightSize / 2 - textSizing / 2)
+        paint.color=ContextCompat.getColor(context, R.color.colorAccent)
+        canvas.drawArc(RectF(0f, 0f, textSizing, textSizing), 0F, 180f, true,  paint)
+        canvas.restore()
+    }
+
+    private fun drawBackgroundColor(canvas: Canvas) {
+        paint.color = ContextCompat.getColor(context, R.color.colorPrimary)
+        canvas.drawRect(0f, 0f, widthSize.toFloat(), heightSize.toFloat(), paint)
+
+    }
+
+    private fun drawProgressBackground(canvas: Canvas?) {
+        paint.color = ContextCompat.getColor(context, R.color.colorPrimaryDark)
+        canvas?.drawRect(0f, 0f, widthSize.toFloat()/2, heightSize.toFloat(), paint)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
